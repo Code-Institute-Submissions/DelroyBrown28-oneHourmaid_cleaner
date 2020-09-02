2bbbb4468f600b1426a428098e3861afb442226f
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-09-02 17:12:10 +0100

    removed cache

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-#os.environ.setdefault("IP",  "0.0.0.0")
-# os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "GBJu2lioq6uv1TrI")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://root:GBJu2lioq6uv1TrI@myfirstcluster.obg5p.mongodb.net/oneHourmaid?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "oneHourmaid")
